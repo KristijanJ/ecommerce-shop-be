62d9b5bbbbe4181957733102bd7c94fa891516d1
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2026-02-11 20:39:12 +0100

    test item api, docker file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import express, { Application } from "express";
+import { prisma } from "./lib/prisma.js";
 
 const app: Application = express();
 const port = "3000";
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
   console.log("Response sent");
   return res.json({ data: "OK" }).status(200);
+});
+
+app.get("/test-item", async (req, res) => {
+  const allTestItems = await prisma.testItem.findMany();
+  console.log("All test items:", JSON.stringify(allTestItems, null, 2));
+  return res.json({ data: allTestItems }).status(200);
+});
+
+app.post("/test-item", async (req, res) => {
+  const text = req.body?.text;
+  if (typeof text != "string") {
+    return res.json({ msg: "invalid text field, must be string" }).status(400);
+  }
+
+  await prisma.testItem.create({
+    data: {
+      text: text
+    }
+  });
+  return res.json({ data: "ok" }).status(200);
 });
 
 app.listen(port, () => {
