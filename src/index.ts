c2f85d4f6da6f49c95444d8d210dbe69c5408d71
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2026-02-11 21:47:15 +0100

    add cors, update route

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
+import cors from "cors";
 import express, { Application } from "express";
+
 import { prisma } from "./lib/prisma.js";
 
 const app: Application = express();
 const port = "3000";
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/health", (req, res) => {
   return res.json({ data: "OK" }).status(200);
 });
 
-app.get("/test-item", async (req, res) => {
+app.get("/test-items", async (req, res) => {
   const allTestItems = await prisma.testItem.findMany();
   console.log("All test items:", JSON.stringify(allTestItems, null, 2));
   return res.json({ data: allTestItems }).status(200);
 });
 
-app.post("/test-item", async (req, res) => {
+app.post("/test-items", async (req, res) => {
   const text = req.body?.text;
   if (typeof text != "string") {
     return res.json({ msg: "invalid text field, must be string" }).status(400);
@@ -24,8 +27,8 @@ app.post("/test-item", async (req, res) => {
 
   await prisma.testItem.create({
     data: {
-      text: text
-    }
+      text: text,
+    },
   });
   return res.json({ data: "ok" }).status(200);
 });
